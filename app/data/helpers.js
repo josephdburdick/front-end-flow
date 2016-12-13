dde58f8e4f3f99dd31e2f6022c0e9a3ce2d14a76
Joe Burdick <josephdburdick@gmail.com>
2016-12-13 16:05:36 -0500

    Compartmentalize template data files into folders.

--- app/data/helpers.js
+++ app/data/helpers.js
@@ -1,5 +1,5 @@
-import moment from 'moment';
-
-export default {
-  formatDate: (date) => moment(new Date(date)).format('MMMM Do')
-}
+// import moment from 'moment';
+//
+// export default {
+//   formatDate: (date) => moment(new Date(date)).format('MMMM Do')
+// }
